c676e175692c4b3c770e9e1baaf8bc476467f6dd
lucasgomes14 <lucazmatehus14@gmail.com>
2026-02-19 17:32:31 -0300

    fix - command domain

--- src/main/java/com/espetinho_da_paula/espetinho/domain/Command.java
+++ src/main/java/com/espetinho_da_paula/espetinho/domain/Command.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -31,6 +32,8 @@ public class Command {
 
     private BigDecimal totalValue;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "command", cascade = CascadeType.ALL, orphanRemoval = true)
