5af3187f9d726974c259aa5f203b48c8e8158f22
lucasgomes14 <lucazmatehus14@gmail.com>
2026-02-19 16:29:45 -0300

    first commit

---
+++ src/main/java/com/espetinho_da_paula/espetinho/EspetinhoApplication.java
@@ -0,0 +1,13 @@
+package com.espetinho_da_paula.espetinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspetinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspetinhoApplication.class, args);
+	}
+
+}
