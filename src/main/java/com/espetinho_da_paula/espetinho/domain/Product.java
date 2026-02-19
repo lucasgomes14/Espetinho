55b9de66522cb94d2552dfcf02040ea1b5ee9dcf
lucasgomes14 <lucazmatehus14@gmail.com>
2026-02-19 16:53:13 -0300

    feat - product domain

---
+++ src/main/java/com/espetinho_da_paula/espetinho/domain/Product.java
@@ -0,0 +1,26 @@
+package com.espetinho_da_paula.espetinho.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_product")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+}
