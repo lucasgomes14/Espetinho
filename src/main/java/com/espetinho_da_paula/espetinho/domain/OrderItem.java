61e1884fadddea5fcc1ef629e61db134b6c30ccb
lucasgomes14 <lucazmatehus14@gmail.com>
2026-02-19 17:27:38 -0300

    feat - orderItem domain

---
+++ src/main/java/com/espetinho_da_paula/espetinho/domain/OrderItem.java
@@ -0,0 +1,33 @@
+package com.espetinho_da_paula.espetinho.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "tb_order_item")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "command_id", nullable = false)
+    @JsonBackReference
+    private Command command;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+
+}
